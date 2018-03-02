a4c2af406c1f1852ec2212de0b3ba9eba2e6f193
yang-hoon <ssang276@sk.com>
2018-03-02 18:44:48 +0900

    aa

---
+++ sample-demo/src/main/java/rest/spring/web/demo/DemoApplication.java
@@ -0,0 +1,12 @@
+package rest.spring.web.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
